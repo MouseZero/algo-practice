c733181195dfc4b6cdc9e81ddf2c101ae52bcc35
MouseZero <rm@mousezero.com>
2018-08-29 14:58:42 -0700

    finish Excel Sheet Column Number

--- test/171/index.js
+++ test/171/index.js
@@ -5,8 +5,12 @@ const base = 26
 
 var titleToNumber = function(s) {
   let answer = 0
-  answer = s.charCodeAt(0) - asciiOffset
-  return s.charCodeAt(0) - asciiOffset
+  for(let i=0; i<s.length; i++) {
+    const multiplier = Math.pow(base, i)
+    const toAdd = (s.charCodeAt(s.length-1-i) - asciiOffset) * multiplier
+    answer += toAdd
+  }
+  return answer
 }
 
 describe('Excel Sheet Column Number', () => {
@@ -15,8 +19,13 @@ describe('Excel Sheet Column Number', () => {
     expect(answer).to.equal(19)
   })
 
-  it('can convert multiple letters', () => {
+  it('works with AB', () => {
     const answer = titleToNumber('AB')
     expect(answer).to.equal(28)
+  })
+
+  it('can convert multiple letters', () => {
+    const answer = titleToNumber('AAAA')
+    expect(answer).to.equal(18279)
   })
 })
